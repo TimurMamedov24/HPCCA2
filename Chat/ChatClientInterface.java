bdefb8ed7d75fd1b5ac2831fd00b27022e522b85
TimMamedov <tm410@exeter.ac.uk>
2019-04-27 16:44:04 +0100

    Chat added

---
+++ Chat/ChatClientInterface.java
@@ -0,0 +1,8 @@
+package Chat;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+public interface ChatClientInterface extends Remote {
+    public void messageFromServer(String message) throws RemoteException;
+
+    public void updateUserList(String[] currentUsers) throws RemoteException;
+}
